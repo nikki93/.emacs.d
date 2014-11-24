805682544577bfd9efdcda0c3a267c0a6dfaa6d6
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-11-23 22:28:00 -0500

    don't use cider for now

--- init.el
+++ init.el
@@ -503,21 +503,21 @@
 
 ;; --- cider ------------------------------------------------------------------
 
-(require-package 'cider)
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
+;; (require-package 'cider)
+;; (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
-(require-package 'ac-cider)
-(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
-(add-hook 'cider-mode-hook 'ac-cider-setup)
-(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-(add-hook 'cider-repl-mode-hook 'auto-complete-mode)
-(eval-after-load "auto-complete"
-                 '(add-to-list 'ac-modes 'cider-mode))
+;; (require-package 'ac-cider)
+;; (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+;; (add-hook 'cider-mode-hook 'ac-cider-setup)
+;; (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+;; (add-hook 'cider-repl-mode-hook 'auto-complete-mode)
+;; (eval-after-load "auto-complete"
+;;                  '(add-to-list 'ac-modes 'cider-mode))
 
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; (defun set-auto-complete-as-completion-at-point-function ()
+;;   (setq completion-at-point-functions '(auto-complete)))
+;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 ;; --- paredit ----------------------------------------------------------------
 
